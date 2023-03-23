a4e22217c6246d4f19e74ea9ea342aeb966af3d5
elisa181205 <elisandradsantos@gmail.com>
2023-03-23 11:19:38 -0300

    Funcional

--- index.js
+++ index.js
@@ -3,59 +3,123 @@ function infoBuscar(){
     const texto = document.getElementById('texto').value;
     const conteudo = document.getElementById('conteudo')
     const DIV0 = document.getElementsByClassName('DIV0')
+    let inicio= 50
 
     const r =texto.replace(" ","%20")
     
-    const url = `https://api.mercadolibre.com/sites/MLB/search?q=${r}&offset=50`;
-    console.log(url)
+    const urlbase = `https://api.mercadolibre.com/sites/MLB/search?q=${r}`;
     const headers ={
         "Content-Type": " application/json"
     }
 
-    fetch(url, {method: "GET". headers})
-    .then(response => response.json())
-    .then(response =>{
-        // console.log(response.results)
-        console.log(response)
-        let anuncios = response.results
+    fetch(urlbase, {method: "GET".headers})
+    .then(base => base.json())
+    .then(base => {
+        console.log((base.paging.total)/50)
+        const page = (base.paging.total)/50
 
-    for (let x = 0; x < 50; x++) {
-        let anuncio = anuncios[x];
-      
-        let linha = document.createElement("tr");
-      
-        let id = document.createElement("td");
-        id.innerHTML = anuncio.id;
-        linha.appendChild(id);
-      
-        let title = document.createElement("td");
-        title.innerHTML = anuncio.title;
-        linha.appendChild(title);
+        fetch(urlbase, {method: "GET". headers})
+            .then(response => response.json())
+            .then(response =>{
+                // console.log(response.results)
+                console.log(response)
+                let anuncios = response.results
+        
+            for (let x = 0; x < 50; x++) {
+                let anuncio = anuncios[x];
+              
+                let linha = document.createElement("tr");
+              
+                // let id = document.createElement("td");
+                // id.innerHTML = anuncio.id;
+                // linha.appendChild(id);
+              
+                let title = document.createElement("td");
+                title.innerHTML = anuncio.title;
+                linha.appendChild(title);
+        
+                let price = document.createElement("td")
+                price.innerHTML = "R$ "+ anuncio.price;
+                linha.appendChild(price)
+        
+                let link = document.createElement("td");
+                let a = document.createElement("a");
+                a.href = anuncio.permalink;
+                a.textContent = anuncio.permalink;
+                link.appendChild(a);
+                linha.appendChild(link);
+        
+                let seller = document.createElement("td")
+                seller.innerHTML=anuncio.seller.nickname;
+                linha.appendChild(seller)
+        
+                let imagem = document.createElement("td")
+                imagem.innerHTML = `<img src="${anuncio.thumbnail}">` //anuncio.thumbnail
+                linha.appendChild(imagem)
+              
+                conteudo.appendChild(linha);
+              }
+              
+        
+            })
+    
 
-        let price = document.createElement("td")
-        price.innerHTML = "R$ "+ anuncio.price;
-        linha.appendChild(price)
+        for(controle=0;controle<page;controle++){
+            const url = `https://api.mercadolibre.com/sites/MLB/search?q=${r}&offset=${inicio}`;
+            console.log(inicio)
 
-        let link = document.createElement("td");
-        let a = document.createElement("a");
-        a.href = anuncio.permalink;
-        a.textContent = anuncio.permalink;
-        link.appendChild(a);
-        linha.appendChild(link);
+            fetch(url, {method: "GET". headers})
+            .then(response => response.json())
+            .then(response =>{
+                // console.log(response.results)
+                console.log(response)
+                let anuncios = response.results
+        
+            for (let x = 0; x < 50; x++) {
+                let anuncio = anuncios[x];
+              
+                let linha = document.createElement("tr");
+              
+                // let id = document.createElement("td");
+                // id.innerHTML = anuncio.id;
+                // linha.appendChild(id);
+              
+                let title = document.createElement("td");
+                title.innerHTML = anuncio.title;
+                linha.appendChild(title);
+        
+                let price = document.createElement("td")
+                price.innerHTML = "R$ "+ anuncio.price;
+                linha.appendChild(price)
+        
+                let link = document.createElement("td");
+                let a = document.createElement("a");
+                a.href = anuncio.permalink;
+                a.textContent = anuncio.permalink;
+                link.appendChild(a);
+                linha.appendChild(link);
+        
+                let seller = document.createElement("td")
+                seller.innerHTML=anuncio.seller.nickname;
+                linha.appendChild(seller)
+        
+                let imagem = document.createElement("td")
+                imagem.innerHTML = `<img src="${anuncio.thumbnail}">` //anuncio.thumbnail
+                linha.appendChild(imagem)
+              
+                conteudo.appendChild(linha);
+              }
+              
+        
+            })
 
-        let seller = document.createElement("td")
-        seller.innerHTML=anuncio.seller.nickname;
-        linha.appendChild(seller)
+            inicio=inicio+50
 
-        let imagem = document.createElement("td")
-        imagem.innerHTML = `<img src="${anuncio.thumbnail}">` //anuncio.thumbnail
-        linha.appendChild(imagem)
-      
-        conteudo.appendChild(linha);
-      }
-
+        }
 
     })
+
+    
     
 
 }
--- teste.js
+++ teste.js
@@ -1,22 +1,61 @@
-function teste(){
-const pessoas = [
-  { nome: "João", idade: 25, cidade: "São Paulo" },
-  { nome: "Maria", idade: 32, cidade: "Rio de Janeiro" },
-  { nome: "Pedro", idade: 20, cidade: "Belo Horizonte" },
-];
 
-const pessoasContainer = document.getElementById("pessoas-container");
+function infoBuscar(){
+  const texto = document.getElementById('texto').value;
+  const conteudo = document.getElementById('conteudo')
+  const DIV0 = document.getElementsByClassName('DIV0')
 
-for (let i = 0; i < pessoas.length; i++) {
-  const pessoa = pessoas[i];
+  const r =texto.replace(" ","%20")
   
-  const pessoaDiv = document.createElement("div");
-  pessoaDiv.innerHTML = `
-    <h3>${pessoa.nome}</h3>
-    <p>Idade: ${pessoa.idade}</p>
-    <p>Cidade: ${pessoa.cidade}</p>
-  `;
+  const url = `https://api.mercadolibre.com/sites/MLB/search?q=${r}&offset=50`;
+  console.log(url)
+  const headers ={
+      "Content-Type": " application/json"
+  }
+
+  fetch(url, {method: "GET". headers})
+  .then(response => response.json())
+  .then(response =>{
+      // console.log(response.results)
+      console.log(response)
+      let anuncios = response.results
+
+  for (let x = 0; x < 50; x++) {
+      let anuncio = anuncios[x];
+    
+      let linha = document.createElement("tr");
+    
+      // let id = document.createElement("td");
+      // id.innerHTML = anuncio.id;
+      // linha.appendChild(id);
+    
+      let title = document.createElement("td");
+      title.innerHTML = anuncio.title;
+      linha.appendChild(title);
+
+      let price = document.createElement("td")
+      price.innerHTML = "R$ "+ anuncio.price;
+      linha.appendChild(price)
+
+      let link = document.createElement("td");
+      let a = document.createElement("a");
+      a.href = anuncio.permalink;
+      a.textContent = anuncio.permalink;
+      link.appendChild(a);
+      linha.appendChild(link);
+
+      let seller = document.createElement("td")
+      seller.innerHTML=anuncio.seller.nickname;
+      linha.appendChild(seller)
+
+      let imagem = document.createElement("td")
+      imagem.innerHTML = `<img src="${anuncio.thumbnail}">` //anuncio.thumbnail
+      linha.appendChild(imagem)
+    
+      conteudo.appendChild(linha);
+    }
+
+
+  })
   
-  pessoasContainer.appendChild(pessoaDiv);
-}
+
 }
